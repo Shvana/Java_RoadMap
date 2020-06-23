b83f4cd2651cf99157845388d7b827cdf8e29f0d
Shvana <htsunemikuv9@gmail.com>
2020-06-23 14:30:38 -0500

    Add a Boolean Variable to End a Loop

---
+++ src/loops/Boolean_Variable.java
@@ -0,0 +1,26 @@
+package loops;
+/*
+ * Using a Boolean Variable to End a Loop
+ */
+
+import java.util.Scanner;
+
+public class Boolean_Variable {
+    public static void main(String[] args){
+        System.out.println("Enter nonnegative numbers.");
+        System.out.println("Place a negative number at the end");
+        System.out.println("to serve as an end marker.");
+
+        int sum = 0;
+        boolean areMore = true;
+        Scanner keyboard = new Scanner(System.in);
+        while (areMore){
+            int next = keyboard.nextInt();
+            if (next <0)
+                areMore = false;
+            else
+                sum = sum +next;
+        }
+        System.out.println("The sum of the numbers is " + sum);
+    }
+}
